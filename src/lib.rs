52e34ee7b959fe493416b4c226f70bf4baabab56
irieryota <ryotairie123@gmail.com>
2020-12-13 02:02:19 +0900

    feat: do updating data on anothe thread

--- src/lib.rs
+++ src/lib.rs
@@ -3,60 +3,55 @@
 use tokio::{sync::RwLock};
 use std::sync::Arc;
 use async_trait::async_trait;
+use std::marker::{Send,Sync};
 
-struct CacheHolder<D,M: ResponseMapper<D>> {
-    endpoint: &'static str,
+
+struct CacheHolder<D: Send + Sync + std::fmt::Debug,F: DataFetcher<D> + Send + Sync> {
     // use Arc to avoid cloning D
-    data: RwLock<Arc<Option<D>>>,
-    mapper: M,
+    data: Arc<RwLock<Arc<Option<D>>>>,
+    fetcher: F,
     interval: u64
 }
 
-pub trait ResponseMapper<Data>{
-    type Resp;
-    fn to_data(resp: Self::Resp) -> Data;
-}
-
 #[async_trait]
-pub trait DataFetcher<D: for<'de> serde::Deserialize<'de>,E> {
-    async fn fetch() -> Result<D,E>; 
+pub trait DataFetcher<D> {
+    type E;
+    async fn fetch(&self) -> Result<D,Self::E>; 
 }
 
-impl <D: for<'de> serde::Deserialize<'de>,M: ResponseMapper<D>> CacheHolder<D,M> {
+impl <D: 'static + Send + Sync + std::fmt::Debug,F: 'static + DataFetcher<D> + Send + Sync> CacheHolder<D,F> {
 
-    pub async fn new(endpoint: &'static str,mapper: M,interval: u64) -> Result<Self,reqwest::Error> {
-        let mut rorator = CacheHolder{
-            endpoint,
-            data: RwLock::new(Arc::new(None)),
-            mapper,
+    pub async fn new(fetcher: F,interval: u64) -> Result<Self,F::E> {
+        let rorator = CacheHolder{
+            data: Arc::new(RwLock::new(Arc::new(None))),
+            fetcher: fetcher,
             interval,
         };
-        rorator.update_data().await?;
+        //rorator.update_data().await?;
         Ok(rorator)
     }
 
-    pub async fn rotate(&mut self) -> Result<Self,reqwest::Error> {
-        loop {
-            let _ = tokio::time::delay_for(std::time::Duration::new(self.interval, 0));
-            self.update_data().await?;
-        }
+    pub fn rotate(self: Arc<Self>) -> () {
+        tokio::spawn(async move {
+            println!("spawned");
+            loop {
+                println!("running...");
+                let _ = tokio::time::sleep(std::time::Duration::new(self.interval, 0)).await;
+                self.update_data().await;
+            }
+        });
     }
 
     pub async fn read_data(&self) -> Arc<Option<D>> {
-        self.data.read().await.clone()
+        let data = self.data.read().await;
+        (*data).clone()
     }
 
-    async fn update_data(&mut self) -> Result<(),reqwest::Error> {
-        let new_data = self.get_new_data().await?;
+    async fn update_data(&self) -> Result<(),F::E> {
+        let new_data = self.fetcher.fetch().await?;
         let mut data = self.data.write().await;
         *data = Arc::new(Some(new_data));
         Ok(())
-    }
-
-    async fn get_new_data(&self) -> Result<D,reqwest::Error> {
-        let resp = reqwest::get(self.endpoint).await?;
-        let resp = resp.json().await?;
-        Ok(resp)
     }
     
 }
@@ -64,8 +59,44 @@ impl <D: for<'de> serde::Deserialize<'de>,M: ResponseMapper<D>> CacheHolder<D,M>
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+
+    #[derive(Debug)]
+    struct E {
+
+    }
+
+    #[tokio::test]
+    async fn it_works() ->  std::result::Result<(),E>{
+
+        struct Fetcher {}
+
+        #[derive(Clone,Debug)]
+        struct Data {
+            a: i64,
+        }
+
+        use async_trait::async_trait;
+     
+        #[async_trait]
+        impl crate::DataFetcher<Data> for Fetcher {
+            type E = E;
+            async fn fetch(&self) -> Result<Data,Self::E> {
+                Ok(Data{a: 100})
+            }
+        }
+
+        let fetcher = Fetcher{};
+
+        let holder = std::sync::Arc::new(crate::CacheHolder::new(fetcher,1).await?);
+        let value = holder.clone().read_data().await;
+        println!("{:?}",value);
+        holder.clone().rotate(); 
+
+        std::thread::sleep(std::time::Duration::new(2, 0));
+        let value2 = holder.clone().read_data().await;
+
+        println!("{:?}",value2);
+
+        Ok(())
     }
 }
