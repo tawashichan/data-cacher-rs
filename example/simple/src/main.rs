9c4dac5cc5cbb307c762f25ba7828c592859d7fc
irieryota <ryotairie123@gmail.com>
2020-12-13 02:12:38 +0900

    chore: add example

--- example/simple/src/main.rs
+++ example/simple/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use data_cacher_rs;
+
+#[tokio::main]
+async fn main() {
+    struct Fetcher {}
+
+    #[derive(Debug)]
+    struct E {
+
+    }
+    #[derive(Clone,Debug)]
+    struct Data {
+        a: i64,
+    }
+
+    use async_trait::async_trait;
+ 
+    #[async_trait]
+    impl data_cacher_rs::DataFetcher<Data> for Fetcher {
+        type E = E;
+        async fn fetch(&self) -> Result<Data,Self::E> {
+            Ok(Data{a: 100})
+        }
+    }
+
+    let fetcher = Fetcher{};
+
+    let holder = std::sync::Arc::new(data_cacher_rs::CacheHolder::new(fetcher,1).await.unwrap());
+    let value = holder.clone().read_data().await;
+    println!("{:?}",value);
+    holder.clone().rotate(); 
+
+    std::thread::sleep(std::time::Duration::new(2, 0));
+    let value2 = holder.clone().read_data().await;
+
+    println!("{:?}",value2);
+
 }
